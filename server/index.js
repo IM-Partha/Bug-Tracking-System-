79947ab8e0db9c8838c75e7225faaeb5232643e8
Partha Debnath <85936227+IM-Partha@users.noreply.github.com>
2025-08-25 09:20:57 +0530

    vercel Add

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 index.use(cors());
 index.use(express.json());
-
+index.get('ping',(req,res)=>{
+  res.send("Pong")
+})
 index.use('/api/auth', authRoutes);
 index.use('/api/projects',authenticate, projectRoutes);
 index.use('/api/issues',authenticate, issueRoutes);
